77c56c524154793625cc0d9f1327519cf7d63ba3
Aarit-cpu <76479740+Aarit-cpu@users.noreply.github.com>
2021-02-12 18:15:11 +0530

    Add files via upload

---
+++ crumpledPapertemplate-main/dustbingreen.js
@@ -0,0 +1,7 @@
+class dustbingreen extends dustbin
+{
+    constructor(x,y){
+        super(x,y)
+        this.image = loadImage("trashcangreen.png")
+    }
+}
---
+++ crumpledPapertemplate-main/paper.js
@@ -0,0 +1,14 @@
+class paper
+{
+    constructor(x,y){
+        var options = {
+            isStatic:false,
+            friction:0.3,
+            density:1.2,
+            restitution:0.3,
+        }
+
+      this.image = loadImage("paper.png")
+    }
+}
+   
